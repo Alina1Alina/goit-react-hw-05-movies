177da7d28c575e5cd5d14fbb25806ffa02bfc12e
Alina1Alina <119854810+Alina1Alina@users.noreply.github.com>
2023-04-27 17:24:25 +0300

    hh

--- src/components/MoviesDetails/MoviesDetails.styled.jsx
+++ src/components/MoviesDetails/MoviesDetails.styled.jsx
@@ -1,6 +1,5 @@
 import { NavLink } from 'react-router-dom';
 import styled from 'styled-components';
-import MoviesDetails from './MoviesDetails';
 
 export const ImageWraper = styled.div`
   width: 250px;
